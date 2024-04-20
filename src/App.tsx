4150143df8fab737c4fcc09e664e66fb062b8154
Angga Pixa <anggas25@live.com>
2024-04-20 15:19:10 +0700

    add index route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import react from 'react'
-import {Routes, BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Scene1 from './views/Scene1'
-import Scene2 from './views/Scene2'
-import Scene3 from './views/Scene3'
+import Scene1 from './views/Scene1';
+import Scene2 from './views/Scene2';
+import Scene3 from './views/Scene3';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Scene1 />} />
       <Route path='/scene1' element={<Scene1 />} />
       <Route path='/scene2' element={<Scene2 />} />
       <Route path='/scene3' element={<Scene3 />} />
