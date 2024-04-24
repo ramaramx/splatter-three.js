94fcd03b8b9440c9b47c5c4dc9a9f94bb1952649
ramaramx <rambudiarto@gmail.com>
2024-04-24 18:53:46 +0700

    update home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Home from './views/home'
 import Scene1 from './views/Scene1';
 import Scene2 from './views/Scene2';
 import Scene3 from './views/Scene3';
@@ -7,7 +7,7 @@ import Scene3 from './views/Scene3';
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Scene1 />} />
+      <Route path="/" element={<Home />} />
       <Route path='/scene1' element={<Scene1 />} />
       <Route path='/scene2' element={<Scene2 />} />
       <Route path='/scene3' element={<Scene3 />} />
---
+++ src/views/home.tsx
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Home() {
+    return(
+        <>
+        <div>Test</div>
+        </>
+    )
+}
+
+export default Home
