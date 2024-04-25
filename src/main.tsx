44bdfea45b6927f80404ce600879889ed58ce1d5
ramaramx <rambudiarto@gmail.com>
2024-04-25 19:33:31 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import Scene1  from './views/Scene1.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/scene1",
+    element: <Scene1 />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
